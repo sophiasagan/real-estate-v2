754ee0ebb0151b6361fbfbc1db87055a4f012326
Sophia Jung <sophiabraddock82@gmail.com>
2021-02-28 13:26:47 -0500

    info middle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Dropdown from "./components/Dropdown";
 import Hero from "./components/Hero";
+import InfoSection from "./components/InfoSection";
 import Navbar from "./components/Navbar";
+import { InfoData, InfoDataTwo } from "./data/InfoData";
 import { SliderData } from "./data/SliderData";
 import GlobalStyle from "./globalStyles";
 
 function App() {
-  const[isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => {
-    setIsOpen(!isOpen)
-  }
+    setIsOpen(!isOpen);
+  };
+
+  const reset = () => {
+    setIsOpen(false);
+  };
 
   return (
     <>
       <GlobalStyle />
-      <Navbar toggle={toggle}/>
-      <Dropdown isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} reset={reset} />
       <Hero slides={SliderData} />
+      <InfoSection {...InfoData} />
+      <InfoSection {...InfoDataTwo} />
     </>
   );
 }
---
+++ src/data/InfoData.js
@@ -0,0 +1,21 @@
+import ImgOne from '../images/House-1.jpg'
+import ImgTwo from '../images/House-2.jpg'
+
+export const InfoData = {
+    heading: 'Explore our beautiful homes',
+    paragraphOne: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, illo delectus ex cupiditate porro ad.',
+    paragraphTow: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus id deserunt obcaecati nisi maxime voluptas.',
+    buttonLabel: 'View Homes',
+    image: ImgOne,
+    reverse: false,
+    delay: 100,
+}
+export const InfoDataTwo = {
+    heading: 'Modern Designs',
+    paragraphOne: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, illo delectus ex cupiditate porro ad.',
+    paragraphTwo: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus id deserunt obcaecati nisi maxime voluptas.',
+    buttonLabel: 'View Homes',
+    image: ImgTwo,
+    reverse: true,
+    delay: 100,
+}
